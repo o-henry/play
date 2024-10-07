9fde1c446159e82df66e50622725bc72452998ae
Henry <c.henry.9209@gmail.com>
2024-10-07 13:18:42 +0900

    Initial Commit

---
+++ A/AApp.swift
@@ -0,0 +1,17 @@
+//
+//  AApp.swift
+//  A
+//
+//  Created by Henry on 10/7/24.
+//
+
+import SwiftUI
+
+@main
+struct AApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ATests/ATests.swift
@@ -0,0 +1,17 @@
+//
+//  ATests.swift
+//  ATests
+//
+//  Created by Henry on 10/7/24.
+//
+
+import Testing
+@testable import A
+
+struct ATests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
